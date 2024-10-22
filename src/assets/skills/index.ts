a2d7b6c1da6e418283f47c03901130db025a73e1
Tirth chokshi <chokshitirth4@gmail.com>
2024-10-22 19:03:02 +0530

    Lint error solved

--- src/assets/skills/index.ts
+++ src/assets/skills/index.ts
@@ -1,18 +1,41 @@
-import docker from "./docker.svg";
+
+
 import postgresssql from "./elephant.png";
-import git from "./Git-Icon-1788C.svg";
-import go from "./php.svg";
+import rust from "./java.svg";
 import mongodb from "./mongodb.svg";
 import mysql from "./mysql.svg";
+import go from "./php.svg";
+import python from "./python-logo-only.png";
+import docker from "./docker.svg";
+import git from "./Git-Icon-1788C.svg";
 import nextjs from "./nextjs-transparent.svg";
 import nodejs from "./nodejs.svg";
-import python from "./python-logo-only.png";
 import react from "./react.svg";
 import redis from "./redis-logo-full-color-rgb.svg";
 import redux from "./redux.svg";
-import rust from "./java.svg";
 import socketio from "./Socket-io.svg";
 import tailwind from "./tailwind.svg";
 import ts from "./ts.svg";
 import vercel from "./vercel.svg";
-export { go, git, postgresssql, rust, redis, mongodb, mysql, socketio, nextjs, nodejs, react, redux, tailwind, ts, vercel, docker, python };
+
+// PNG and other image imports
+
+export {
+    docker,
+    git,
+    go,
+    mongodb,
+    mysql,
+    nextjs,
+    nodejs,
+    postgresssql,
+    python,
+    react,
+    redis,
+    redux,
+    rust,
+    socketio,
+    tailwind,
+    ts,
+    vercel,
+};
